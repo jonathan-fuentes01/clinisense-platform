b673be975bf1717675754013270ef945c1ddace8
jonathan <jonathanfuentes2021@gmail.com>
2026-02-20 01:06:04 -0800

    Added ble.h

--- firmware/esp32_nano_uart_ble/src/ble.h
+++ firmware/esp32_nano_uart_ble/src/ble.h
@@ -1,5 +1,10 @@
 #ifndef BLE_SERVER_CLIENT
 #define BLE_SERVER_CLIENT
+
+// define 128-bit UUID values using (https://www.uuidgenerator.net/)
+#define SERVICE_UUID 
+
 void bleTask(void *pvParameters);
 void setupBLE();
+
 #endif
