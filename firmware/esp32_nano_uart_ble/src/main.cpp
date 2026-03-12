6025962854b8753f78ef86d5f36d0731c508cc56
jonathan <jonathanfuentes2021@gmail.com>
2026-03-11 20:21:51 -0700

    Added the tasks and queue creations in the setup function.

--- firmware/esp32_nano_uart_ble/src/main.cpp
+++ firmware/esp32_nano_uart_ble/src/main.cpp
@@ -1,18 +1,49 @@
-#include <Arduino.h>
 #include <ArduinoJson.h>
 #include <NimBLEDevice.h>
+#include "queues.h"
 #include "uart.h"
-#include "ble.cpp"
+#include "ble.h"
 #include "data_processing.h"
-
-// initialize queue handles
-QueueHandle_t uartQueue;
-QueueHandle_t bleQueue;
+#include <stdexcept>
+#include <iostream>
 
 void setup() {
     Serial.begin(115200);
-}
 
-void loop() {
+    phQueue = xQueueCreate(5, sizeof(uint32_t));
+    bleQueue = xQueueCreate(5, 128);
 
+    if (bleQueue == NULL){
+        throw std::runtime_error("The BLE queue has not been created.");
+    }
+    if (phQueue == NULL){
+        throw std::runtime_error("The UART queue has not been created.");
+    }
+
+    xTaskCreate(
+        bleTask,
+        "Bluetooth Task",
+        4096,
+        NULL,
+        1,
+        NULL
+    );
+
+    xTaskCreate(
+        uartTask,
+        "UART Task",
+        4096,
+        NULL,
+        1,
+        NULL
+    );
+
+    xTaskCreate(
+        processingTask,
+        "Data Processing Task",
+        4096,
+        NULL,
+        1,
+        NULL
+    );
 }
