aaedb3db3ab6aec53916c7eedd6de624b7e639f6
jonathan <jonathanfuentes2021@gmail.com>
2026-03-11 20:56:56 -0700

    Added callbacks for connect and disconnect for BLE server and set characteristic to notify when data updates.

--- firmware/esp32_nano_uart_ble/src/ble.cpp
+++ firmware/esp32_nano_uart_ble/src/ble.cpp
@@ -1,8 +1,49 @@
+#include "queues.h"
 #include "ble.h"
 #include <NimBLEDevice.h>
 
+class ServerCallbacks : public NimBLEServerCallbacks{
+    void onConnect(NimBLEServer* pServer) {
+        connected = true;
+        Serial.println("Client connected!!");
+    }
+
+    void onDisconnect(NimBLEServer* pServer){
+        connected = false;
+        Serial.println("Client disconnected!!");
+        
+    }
+};
+
 // initialize BLE as GATT server 
 void setupBLE(){
-    NimBLEDevice::init("NanoESP32");
+    
+    NimBLEDevice::init("ESP32 NimBLE");
     NimBLEServer* pServer = NimBLEDevice::createServer(); // create GATT server
+    pServer->setCallbacks( new ServerCallbacks());
+
+    // create your service
+    NimBLEService* pService = pServer->createService(SERVICE_UUID);
+    
+    // create set your characteristic
+    pCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID,
+        NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::NOTIFY // read and notify (notify is updating unsolicited when data changes)
+    );
+}
+
+void bleTask(void *pvParameters){
+    setupBLE();
+    
+    char buffer[BLE_MSG_SIZE];
+
+    while (1){
+        if(xQueueReceive(bleQueue, &buffer, portMAX_DELAY)){
+            pCharacteristic->setValue(buffer);
+            pCharacteristic->notify();
+
+            Serial.print("Notified: ");
+            Serial.println(buffer);
+        }
+    }
 }
--- firmware/esp32_nano_uart_ble/src/ble.h
+++ firmware/esp32_nano_uart_ble/src/ble.h
@@ -1,10 +1,12 @@
-#ifndef BLE_SERVER_CLIENT
-#define BLE_SERVER_CLIENT
+#ifndef BLE_SERVER_CLIENT_H
+#define BLE_SERVER_CLIENT_H
 
 // define 128-bit UUID values using (https://www.uuidgenerator.net/)
 #define SERVICE_UUID "cdbe9e3f-2839-4d61-a1d9-4043e0e0eaeb"
 #define CHARACTERISTIC_UUID "00a81b7a-1fc2-467a-aabc-8bf7a71bbf5c"
 
+bool connected;
+NimBLECharacteristic* pCharacteristic;
 void bleTask(void *pvParameters);
 void setupBLE();
 
