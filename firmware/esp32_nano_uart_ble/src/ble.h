4328ef88ed66ba3fe9b31efd0d2a5d5c44713462
jonathan <jonathanfuentes2021@gmail.com>
2026-03-11 19:30:57 -0700

    Added random service and characteristic uuid and moved QueueHandle_t initialization into header.

--- firmware/esp32_nano_uart_ble/src/ble.h
+++ firmware/esp32_nano_uart_ble/src/ble.h
@@ -2,7 +2,10 @@
 #define BLE_SERVER_CLIENT
 
 // define 128-bit UUID values using (https://www.uuidgenerator.net/)
-#define SERVICE_UUID 
+#define SERVICE_UUID "cdbe9e3f-2839-4d61-a1d9-4043e0e0eaeb"
+#define CHARACTERISTIC_UUID "00a81b7a-1fc2-467a-aabc-8bf7a71bbf5c"
+
+QueueHandle_t bleQueue;
 
 void bleTask(void *pvParameters);
 void setupBLE();
