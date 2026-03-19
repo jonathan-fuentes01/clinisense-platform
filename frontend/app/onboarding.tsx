6110ca6304ee3a2b464bbe83fae107cf83c804e5
viveevu11 <132483072+viveevu11@users.noreply.github.com>
2026-03-18 17:38:50 -0700

    Fix onboarding UI

--- frontend/app/onboarding.tsx
+++ frontend/app/onboarding.tsx
@@ -14,75 +14,87 @@ export default function Onboarding() {
                 paddingHorizontal: 24,
             }}
         >
-            <Image
-                source={require("../assets/images/flask.png")}
+            <View
                 style={{
-                    width: 150,
-                    height: 150,
-                    resizeMode: "contain",
-                    marginBottom: 10,
-                }}
-            />
-            <Text
-                style={{
-                    fontSize: 26,
-                    fontWeight: "bold",
-                    color: "green",
-                }}
-            >
-                CSULB BMES
-            </Text>
-            <Text
-                style={{
+                    width: "100%",
+                    maxWidth: 520,
                     marginTop: 30,
-                    fontSize: 20,
-                    color: "gray",
-                }}
-            >
-                Login to Detect pH
-            </Text>
-
-            <TouchableOpacity
-                onPress={() => router.push("/signin")}
-                style={{
-                    marginTop: 10,
-                    backgroundColor: "green",
-                    paddingVertical: 20,
-                    paddingHorizontal: "35%",
-                    borderRadius: 30,
-                }}
-            >
-                <Text
-                    style={{
-                        color: "white",
-                        fontSize: 16,
-                    }}
-                >
-                    Login
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => router.push("/signup")}
-                style={{
-                    marginTop: 10,
-                    backgroundColor: "white",
-                    borderColor: "green",
-                    borderWidth: 2,
-                    paddingVertical: 20,
-                    paddingHorizontal: "32%",
-                    borderRadius: 30,
+                    gap: 12,
                     alignItems: "center",
                 }}
             >
+                <Image
+                    source={require("../assets/images/flask.png")}
+                    style={{
+                        width: 150,
+                        height: 150,
+                        resizeMode: "contain",
+                        marginBottom: 10,
+                    }}
+                />
                 <Text
                     style={{
+                        fontSize: 26,
+                        fontWeight: "bold",
                         color: "green",
-                        fontSize: 16,
                     }}
                 >
-                    Sign Up
+                    CSULB BMES
                 </Text>
-            </TouchableOpacity>
+                <Text
+                    style={{
+                        marginTop: 30,
+                        fontSize: 20,
+                        color: "gray",
+                    }}
+                >
+                    Login to Detect pH
+                </Text>
+
+                <TouchableOpacity
+                    onPress={() => router.push("/signin")}
+                    style={{
+                        width: "100%",
+                        backgroundColor: "green",
+                        paddingVertical: 20,
+                        alignItems: "center",
+                        borderRadius: 30,
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: "white",
+                            fontSize: 16,
+                            fontWeight: "600",
+                        }}
+                    >
+                        Login
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => router.push("/signup")}
+                    style={{
+                        marginTop: 10,
+                        backgroundColor: "white",
+                        borderColor: "green",
+                        borderWidth: 2,
+                        paddingVertical: 20,
+                        borderRadius: 30,
+                        alignItems: "center",
+                        width: "100%"
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: "green",
+                            fontSize: 16,
+                            fontWeight: "600",
+                        }}
+                    >
+                        Sign Up
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
