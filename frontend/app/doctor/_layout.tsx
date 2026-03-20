e2d698e94ecba8fdd50000e34b0eb81974ef7965
viveevu11 <132483072+viveevu11@users.noreply.github.com>
2026-03-19 17:43:16 -0700

    Added tab icons

--- frontend/app/doctor/_layout.tsx
+++ frontend/app/doctor/_layout.tsx
@@ -1,11 +1,37 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function DoctorLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="patients" options={{ title: "Patients" }} />
-      <Tabs.Screen name="alerts" options={{ title: "Alerts" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+      <Tabs.Screen 
+      name="patients" 
+      options={{ 
+        title: "Patients",
+        tabBarIcon: ({ color, size }) => (
+            <Ionicons name="people" size={size} color={color}/>
+        ),
+       }} 
+       />
+      <Tabs.Screen 
+      name="alerts" 
+      options={{ 
+        title: "Alerts",
+        tabBarIcon: ({ color, size }) => (
+            <Ionicons name="alert"  size={size} color={color}/>
+        )
+       }} 
+       />
+      <Tabs.Screen 
+      name="profile" 
+      options={{ 
+        title: "Profile",
+        tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="user-doctor" size={size} color={color} />
+        )
+       }} 
+       />
     </Tabs>
   );
 }
