c44b61d499d3b5e30ac8646d033ef62821b8f183
viveevu11 <132483072+viveevu11@users.noreply.github.com>
2026-03-19 19:58:02 -0700

    Fixed tab layout repeated lines

--- frontend/app/doctor/_layout.tsx
+++ frontend/app/doctor/_layout.tsx
@@ -32,9 +32,6 @@ export default function DoctorLayout() {
         )
        }} 
        />
-      <Tabs.Screen name="patients" options={{ title: "Patients" }} />
-      <Tabs.Screen name="alerts" options={{ title: "Alerts" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
       <Tabs.Screen name="patient" options={{ href: null }} />
     </Tabs>
   );
