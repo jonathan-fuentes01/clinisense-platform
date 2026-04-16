91d4064c334a32b3d5d926980917fd9454eef8e1
jonathan <jonathanfuentes2021@gmail.com>
2026-04-15 19:10:27 -0700

    Added IIR and FIR filter for UART values

--- firmware/esp32_nano_uart_ble/src/uart.cpp
+++ firmware/esp32_nano_uart_ble/src/uart.cpp
@@ -4,14 +4,47 @@
 #include "uart.h"
 
 
+
 void uartTask(void *pvParameters){
     while (true){
         if (Serial1.available()){
-            
             char raw[UART_MSG_SIZE];
-            Serial1.readBytesUntil('\n', raw, UART_MSG_SIZE); 
-
-            xQueueSend(phQueue, &raw, portMAX_DELAY);
+            Serial1.readBytesUntil('\n', raw, UART_MSG_SIZE);  
+            float sample = atof(raw); // read raw UART byte data
+            float filteredValue = IIRFilter(sample);
+            xQueueSend(phQueue, &filteredValue, portMAX_DELAY);
         }
     }
+}
+
+float IIRFilter(float sample){
+    static float previous; // hold previous sample (y(n-1))
+    
+    // reading first sample w/ flag
+    static bool init = false;
+    if (!init){
+        previous = sample;
+        init = true;
+    }
+
+    // IIR filter: y(n) = (x(n) + y(n-1))/2
+    float output = (sample + previous) / 2.0;
+
+    previous = output; // store past outputs
+    return output;
+}  
+
+float FIRFilter(float sample){
+    static float previous = 0;
+    static bool init = false;
+    if(!init){
+        previous = sample;
+        init = true;
+    } 
+
+    // FIR filter: y(n) = (x(n) + x(n-1))/2
+    float output = (sample + previous) / 2.0;
+    
+    previous = sample; // store past inputs
+    return output;
 }
--- firmware/esp32_nano_uart_ble/src/uart.h
+++ firmware/esp32_nano_uart_ble/src/uart.h
@@ -1,8 +1,10 @@
 #ifndef UART_PH_H
 #define UART_PH_H
-
+#define MEDIAN_SAMPLES 5
 #define BAUD_RATE 115200
 void uartTask(void *pvParameters);
-
+float medianFilter(float sample);
+float FIRFilter(float sample);
+float IIRFilter(float sample);
 #endif
 
