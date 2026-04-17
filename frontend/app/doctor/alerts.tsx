6751276cbc1f4fc6833a86498663b0db0f62c0af
viveevu11 <132483072+viveevu11@users.noreply.github.com>
2026-04-17 16:25:11 -0700

    Minor UI fixes

--- frontend/app/doctor/alerts.tsx
+++ frontend/app/doctor/alerts.tsx
@@ -71,7 +71,7 @@ export default function AlertsPage() {
 
   return (
     <ScrollView
-      style={{ flex: 1, paddingHorizontal: 10, backgroundColor: "#f2f2f2" }}
+      style={{ flex: 1, paddingHorizontal: 24, backgroundColor: "#f2f2f2" }}
       refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
     >
       <Text style={{ fontWeight: "bold", fontSize: 24, marginTop: 15 }}>Alerts</Text>
